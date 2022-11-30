d63781129cad879a81769c08b376f5f327c794d3
AlexanderGrams <99349751+AlexanderGrams@users.noreply.github.com>
2022-11-30 04:13:05 +0300

    feat: create accordion in FAQ

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,15 @@ import "./index.css";
 
 import Advantage from "../components/Advantage.js";
 import Section from "../components/Section.js";
+import Faq from "../components/Faq.js"
 
 import {
   advantageArray,
+  faqArray,
   advantageListSection,
   templateAdvantageSelector,
+  faqAccordion,
+  faqList
 } from "../utils/constants.js";
 
 const advantagesList = new Section(
@@ -27,3 +31,38 @@ const getAdvantage = (item) => {
 };
 
 advantagesList.renderItems(advantageArray);
+
+
+const faqItems = new Section (
+  {
+    renderer: (item) => {
+      const faqElement = getFaq(item);
+
+      faqItems.setItem(faqElement);
+    },
+  },
+  faqList
+)
+
+const getFaq = (item) => {
+  const faq = new Faq(item);
+
+  return faq.getFaqElement();
+};
+
+faqItems.renderItems(faqArray)
+
+// открытие акардиона
+// const faqAccordionArray = Array.from(faqAccordion)
+
+// for (let i = 0; i < faqAccordionArray.length; i++) {
+//   faqAccordionArray[i].addEventListener("click", function() {
+//     this.querySelector(".faq__vector").classList.toggle("faq__vector_active")
+//     const panel = this.nextElementSibling;
+//     if (panel.style.maxHeight) {
+//       panel.style.maxHeight = null;
+//     } else {
+//       panel.style.maxHeight = panel.scrollHeight + "px";
+//     }
+//   });
+// }
