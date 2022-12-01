fd507216876073dfe8b32d5954ebe4c3d117303e
evgeniy balagurov <evgeniy.balagurov@yandex.ru>
2022-12-02 00:16:25 +0400

    fix: fix border radius

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,14 +13,12 @@ import {
   templateAdvantageSelector,
   templateVacancySelector,
   faqArray,
-  faqAccordion,
   faqList,
   galleryMain,
   galleryFilm,
   gallerySlideAll,
-  gallerySlide,
   triggerLeft,
-  triggerRight
+  triggerRight,
 } from "../utils/constants.js";
 
 // advantages
@@ -123,60 +121,61 @@ const getFaq = (item) => {
   return faq.getFaqElement();
 };
 
-
-faqItems.renderItems(faqArray)
-
+faqItems.renderItems(faqArray);
 
 let slideIndex = 0;
 
 function render() {
-  const mainWidth = galleryMain.offsetWidth
-  galleryFilm.style.right = (mainWidth * slideIndex) + (slideIndex * 50) + 'px';
+  const mainWidth = galleryMain.offsetWidth;
+  galleryFilm.style.right = mainWidth * slideIndex + slideIndex * 50 + "px";
 }
 
-function changeSize(){
+function changeSize() {
   const mainWidth = galleryMain.offsetWidth;
-  galleryFilm.style.width = (gallerySlideAll.length - 1) * 50 + mainWidth * gallerySlideAll.length +'px';
-  gallerySlideAll.forEach(slide => slide.style.width = mainWidth + 'px')
+  galleryFilm.style.width =
+    (gallerySlideAll.length - 1) * 50 +
+    mainWidth * gallerySlideAll.length +
+    "px";
+  gallerySlideAll.forEach((slide) => (slide.style.width = mainWidth + "px"));
   render();
 }
 
-window.addEventListener('resize', changeSize);
+window.addEventListener("resize", changeSize);
 changeSize();
 
-function disableTrigger(boolean, trigger){
-  if(boolean){
-    trigger.classList.add('gallery__trigger_disabled');
-    trigger.setAttribute("disabled", '');
-  }else{
-    trigger.classList.remove('gallery__trigger_disabled');
-    trigger.disabled = false
+function disableTrigger(boolean, trigger) {
+  if (boolean) {
+    trigger.classList.add("gallery__trigger_disabled");
+    trigger.setAttribute("disabled", "");
+  } else {
+    trigger.classList.remove("gallery__trigger_disabled");
+    trigger.disabled = false;
   }
 }
 
-triggerLeft.addEventListener('click', ()=>{
-	if (slideIndex > 0){
-		slideIndex--;
-    if(slideIndex === 0){
-      disableTrigger(true, triggerLeft)
-    }else{
-      disableTrigger(false, triggerRight)
+triggerLeft.addEventListener("click", () => {
+  if (slideIndex > 0) {
+    slideIndex--;
+    if (slideIndex === 0) {
+      disableTrigger(true, triggerLeft);
+    } else {
+      disableTrigger(false, triggerRight);
     }
-	}
-	render()
+  }
+  render();
 });
 
-triggerRight.addEventListener('click', (evt)=>{
-  console.log(evt.target)
-	if (gallerySlideAll.length - 1 > slideIndex){
-		slideIndex++;
-    if(gallerySlideAll.length - 1 === slideIndex){
-      disableTrigger(true, triggerRight)
-    }else{
-      disableTrigger(false, triggerLeft)
+triggerRight.addEventListener("click", (evt) => {
+  console.log(evt.target);
+  if (gallerySlideAll.length - 1 > slideIndex) {
+    slideIndex++;
+    if (gallerySlideAll.length - 1 === slideIndex) {
+      disableTrigger(true, triggerRight);
+    } else {
+      disableTrigger(false, triggerLeft);
     }
-	}
-	render()
+  }
+  render();
 });
 
 disableTrigger(true, triggerLeft);
